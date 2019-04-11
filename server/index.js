33e947db7c612c7b989c8f0c4596b7e76648ea2c
Brock Ross <m.brock.ross@gmail.com>
2019-04-10 20:59:32 -0600

    Adds paths to handle ids supplied in url (i.e., "localhost:1337/5/")

--- server/index.js
+++ server/index.js
@@ -6,14 +6,15 @@ const cors = require('cors');
 
 app.use(bodyParser());
 app.use(cors());
+
 app.use(express.static(__dirname + '/../client/dist'));
+app.use('/*/styles.css', express.static(__dirname + '/../client/dist/styles.css'));
+app.use('/*', express.static(__dirname + '/../client/dist'));
 
 app.use((req, res, next) => {
   console.log(`serving a ${req.method} request to ${req.url}.`);
   next()
 })
-
-app.use('/styled-components', express.static(__dirname + '/../node_modules/styled-components/dist/styled-components.browser.esm.js'))
 
 app.listen(PORT, () => {
   console.log(`proxy server listening on port ${PORT}!`);
